ed63425b920b61b6ba39ea2374daadc8d995909a
usman <musmankhan707@gmail.com>
2022-10-30 01:05:51 +0500

    Initial commit

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,82 @@
+import random
+choice=int(input("What do you choose? type 0 for rock, type 1 for paper, type 2 for sccissors \n"))
+rock=('''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+  ''')
+paper=('''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+  ''')
+scissors=('''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+  ''')
+won=('''
+    .-.  _
+    | | / )
+    | |/ /
+   _|__ /_
+  / __)-' )
+  \  `(.-')
+   > ._>-'
+  / \/
+''')
+comp=('''
+     _                         _
+    |_|                       |_|
+    | |         /^^^\         | |
+   _| |_      (| "o" |)      _| |_
+ _| | | | _    (_---_)    _ | | | |_ 
+| | | | |' |    _| |_    | `| | | | |
+\          /   /     \   \          /
+ \        /  / /(. .)\ \  \        /
+   \    /  / /  | . |  \ \  \    /
+     \  \/ /    ||Y||    \ \/  /
+       \_/      || ||      \_/
+                () ()
+                || ||
+               ooO Ooo
+''')
+choice_image=[rock,paper,scissors]
+human_choice=choice_image[choice]
+print(human_choice)
+
+print("Computer chooses")
+
+choice2=random.randint(0,2)
+comp_choice=choice_image[choice2]
+print(comp_choice)
+
+if(choice2==0 and choice==2):
+  print("Computer won Game over!")
+  print(comp)
+elif(choice2==2 and choice==1):
+  print("Computer won Game over!")
+  print(comp)
+elif(choice2==2 and choice==0):
+  print("Computer won Game over!")
+  print(comp)
+elif(choice2==2 and choice==0):
+  print("you won")
+  print(won)
+elif(choice2==1 and choice==2):
+  print("you won")
+  print(won)
+elif(choice2==0 and choice==2):
+  print("you won")
+  print(won)
+else:
+  print("its a tie ☺")
